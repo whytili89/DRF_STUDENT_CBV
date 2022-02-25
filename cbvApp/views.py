a1c651d4a593f0b041dbab6c60c439a11d8c642c
Taeyeon <mindc1891@gmail.com>
2022-02-25 15:47:54 +0900

    modify:Use mixins to modify views.py

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -1,50 +1,32 @@
-from django.http import Http404
 from django.shortcuts import render
 from cbvApp.models import Student
 from cbvApp.serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics,mixins
+from django.http import Http404
 
-# Create your views here.
-class StudentList(APIView):
+class StudentList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    def get(self, request):
-        students = Student.objects.all()
-        serializer = StudentSerializer(students,many=True)
-        return Response(serializer.data)
+    def get(self,request):
+        return self.list(request)
 
     def post(self,request):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
 
-class StudentDetail(APIView):
-
-    def get_object(self,pk):
-        try:
-            return Student.objects.get(pk=pk)
-        except Student.DoesNotExist:
-            raise Http404
+class StudentDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
     def get(self,request,pk):
-        student = self.get_object(pk)
-        serializer = StudentSerializer(student)
-        return Response(serializer.data)
+        return self.retrieve(request,pk)
 
     def put(self,request,pk):
-        student = self.get_object(pk)
-        Serializer = StudentSerializer(student, data=request.data)
-        if Serializer.is_valid():
-            Serializer.save()
-            return Response(Serializer.data)
-        return Response(Serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        return self.update(request,pk)
 
     def delete(self,request,pk):
-        student = self.get_object(pk)
-        student.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
-        
-        
+        return self.destroy(request,pk)
